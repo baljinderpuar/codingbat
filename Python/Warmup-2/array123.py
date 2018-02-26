08ad8b77e36fdabb391ca1c41c200d65660fd899
baljinderpuar <baljinderpuar@gmail.com>
2018-02-25 22:41:00 -0500

    Create array123.py

---
+++ Python/Warmup-2/array123.py
@@ -0,0 +1,7 @@
+# Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+def array123(nums):
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False
