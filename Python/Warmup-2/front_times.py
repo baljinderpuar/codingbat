9d642756f9778d8aa45411ab71d8d6b75029eac2
baljinderpuar <baljinderpuar@gmail.com>
2018-02-25 22:31:41 -0500

    Create front_times.py

---
+++ Python/Warmup-2/front_times.py
@@ -0,0 +1,9 @@
+# Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, 
+# or whatever is there if the string is less than length 3. Return n copies of the front;
+
+def front_times(str, n):
+  first = str[:3]
+  result = "";
+  for i in range(n):
+    result += first;
+  return result
