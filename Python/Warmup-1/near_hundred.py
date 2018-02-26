f9e4242c97295fddcb3ea9b4e55e226aef8b8841
baljinderpuar <baljinderpuar@gmail.com>
2018-02-25 22:23:35 -0500

    Create near_hundred.py

---
+++ Python/Warmup-1/near_hundred.py
@@ -0,0 +1,5 @@
+# Given an int n, return True if it is within 10 of 100 or 200. 
+# Note: abs(num) computes the absolute value of a number.
+
+def near_hundred(n):
+  return abs(200 - n) <= 10 or abs(100 - n) <= 10
