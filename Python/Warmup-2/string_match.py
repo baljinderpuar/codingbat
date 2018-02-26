acc3d52a87c800c2212a369ff4a624b6b206da2e
baljinderpuar <baljinderpuar@gmail.com>
2018-02-25 22:47:43 -0500

    Update string_match.py

--- Python/Warmup-2/string_match.py
+++ Python/Warmup-2/string_match.py
@@ -1,4 +1,4 @@
-#Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. 
+# Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. 
 # So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
 
 def string_match(a, b):
