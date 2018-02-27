2b634d5c46cb5a25117f4a8189732507e167e6b4
baljinderpuar <baljinderpuar@gmail.com>
2018-02-27 05:40:21 -0500

    Create make_out_word.py

---
+++ Python/String-1/make_out_word.py
@@ -0,0 +1,5 @@
+# Given an "out" string length 4, such as "<<>>", and a word, 
+# return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+
+def make_out_word(out, word):
+  return out[0:len(out)/2] + word + out[len(out)/2: len(out)]
