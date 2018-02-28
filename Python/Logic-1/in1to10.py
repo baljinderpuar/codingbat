2a0f96d63e33903620c6a9258bf3f9c29b3fe0f7
baljinderpuar <baljinderpuar@gmail.com>
2018-02-28 08:09:22 -0500

    Create in1to10.py

---
+++ Python/Logic-1/in1to10.py
@@ -0,0 +1,9 @@
+# Given a number n, return True if n is in the range 1..10, inclusive. 
+# Unless outside_mode is True, in which case return True 
+# if the number is less or equal to 1, or greater or equal to 10.
+
+def in1to10(n, outside_mode):
+  if outside_mode:
+    return n <= 1 or n >= 10
+  else:
+    return n >= 1 and n <= 10
