3a54b306e541ea1eded0950f9d91666521cecf34
baljinderpuar <baljinderpuar@gmail.com>
2018-03-01 21:51:44 -0500

    Create notString.java

---
+++ Java/Warmup-1/notString.java
@@ -0,0 +1,11 @@
+/* Given a string, return a new string where "not " has been added to the front. 
+However, if the string already begins with "not", return the string unchanged. 
+Note: use .equals() to compare 2 strings. */
+
+public String notString(String str) {
+  if((str.length() >= 3) && (str.substring(0,3).equals("not"))){
+    return str;
+  } else {
+    return "not " + str;
+  }
+}
