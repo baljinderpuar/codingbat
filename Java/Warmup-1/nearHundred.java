438825ee122f57192ca46fb0b6e7272ca1a138d2
baljinderpuar <baljinderpuar@gmail.com>
2018-03-01 21:49:44 -0500

    Create nearHundred.java

---
+++ Java/Warmup-1/nearHundred.java
@@ -0,0 +1,6 @@
+/* Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number. */
+
+public boolean nearHundred(int n) {
+  return ((Math.abs(100 - n) <= 10) ||
+    (Math.abs(200 - n) <= 10));
+}
