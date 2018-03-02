be1513300f27f960245ac1f6a7557b11a487c306
baljinderpuar <baljinderpuar@gmail.com>
2018-03-01 21:54:43 -0500

    Create backAround.java

---
+++ Java/Warmup-1/backAround.java
@@ -0,0 +1,7 @@
+/* Given a string, take the last char and return a new string with the last char added at the front and back, 
+so "cat" yields "tcatt". The original string will be length 1 or more. */
+
+public String backAround(String str) {
+  String last = str.substring(str.length() - 1);
+  return last + str + last;
+}
