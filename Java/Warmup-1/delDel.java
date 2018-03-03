18d9cbecff2299593e36407122f4d868b7234b52
baljinderpuar <baljinderpuar@gmail.com>
2018-03-02 20:35:03 -0500

    Create delDel.java

---
+++ Java/Warmup-1/delDel.java
@@ -0,0 +1,13 @@
+/* Given a string, if the string "del" appears starting at index 1, 
+return a string where that "del" has been deleted. 
+Otherwise, return the string unchanged. */
+
+public String delDel(String str) {
+  if(str.length() >= 4 && (str.substring(1,4).equals("del"))){
+    String front = str.substring(0,1);
+    String back = str.substring(4);
+    return front + back;
+  } else{
+    return str;
+  }
+}
