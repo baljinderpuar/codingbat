23bbf48bb117541c5de79ecc10298c969a637950
baljinderpuar <baljinderpuar@gmail.com>
2018-03-31 14:21:14 -0400

    Create extraFont.java

---
+++ Java/String-1/extraFont.java
@@ -0,0 +1,11 @@
+/* Given a string, return a new string made of 3 copies of the first 2 chars of the original string. 
+The string may be any length. If there are fewer than 2 chars, use whatever is there. */
+
+public String extraFront(String str) {
+  if(str.length()<2) 
+    return str + str + str;
+  else {
+    String woo = str.substring(0,2);
+    return woo + woo + woo;
+  }
+}
