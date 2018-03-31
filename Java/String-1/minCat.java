c6f62fd65055556ae88c7b063feea3306351bf33
baljinderpuar <baljinderpuar@gmail.com>
2018-03-31 14:20:26 -0400

    Create minCat.java

---
+++ Java/String-1/minCat.java
@@ -0,0 +1,10 @@
+/* Given two strings, append them together (known as "concatenation") and return the result. 
+However, if the strings are different lengths, omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length. */
+
+public String minCat(String a, String b) {
+  if(a.length() > b.length()){
+    return a.substring(a.length()-b.length(), a.length()) + b;
+  } else {
+    return a + b.substring(b.length()-a.length(), b.length());
+  }
+}
