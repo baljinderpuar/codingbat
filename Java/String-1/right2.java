76da2edb5e42fe9ef8cbd211e3ef514d760e1f9b
baljinderpuar <baljinderpuar@gmail.com>
2018-03-31 13:55:53 -0400

    Create right2.java

---
+++ Java/String-1/right2.java
@@ -0,0 +1,9 @@
+/* Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. 
+The string length will be at least 2. */
+
+public String right2(String str) {
+  if(str.length() <= 2) 
+    return str;
+  else 
+    return str.substring(str.length()-2, str.length()) + str.substring(0,str.length()-2);
+}
