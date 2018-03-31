d0b348bfecb6c3515543b7e29aad19ad47b91594
baljinderpuar <baljinderpuar@gmail.com>
2018-03-31 14:01:14 -0400

    Create endsLy.java

---
+++ Java/String-1/endsLy.java
@@ -0,0 +1,8 @@
+/* Given a string, return true if it ends in "ly". */
+
+public boolean endsLy(String str) {
+  return str.endsWith("ly");
+  
+  //if(str.length() < 2) return false;
+  //else return (str.substring(str.length()-2, str.length())).equals("ly");
+}
