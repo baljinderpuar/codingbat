d4c18acea240bde085e2becd2d00710bc10667b2
baljinderpuar <baljinderpuar@gmail.com>
2018-03-31 14:18:09 -0400

    Create frontAgain.java

---
+++ Java/String-1/frontAgain.java
@@ -0,0 +1,12 @@
+/* Given a string, return true if the first 2 chars in the string also appear at the end of the string, 
+such as with "edited". */
+
+public boolean frontAgain(String str) {
+  if(str.length()<2) 
+    return false;
+  else if(str.substring(0,2).equals(str.substring(str.length()-2,str.length()))) 
+    return true;
+  else 
+    return false;
+}
+
