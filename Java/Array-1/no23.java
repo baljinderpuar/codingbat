d7746511d83eb2ef2b99e6f6f7f17fe7944ef533
baljinderpuar <baljinderpuar@gmail.com>
2019-05-20 14:43:40 -0400

    Update no23.java

--- Java/Array-1/no23.java
+++ Java/Array-1/no23.java
@@ -1,8 +1,5 @@
 /* Given an int array length 2, return true if it does not contain a 2 or 3. */
 
 public boolean no23(int[] nums) {
-  if(nums[0] != 2 && nums[0] != 3 && nums[1] != 2 && nums[1] != 3) 
-  	return true;
-  else 
-  	return false;
+  return (nums[0]!=2 && nums[0]!=3 && nums[1]!=2 && nums[1]!=3);
 }
