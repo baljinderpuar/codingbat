b0388c19de92603487c7e01ac260282507de1801
baljinderpuar <baljinderpuar@gmail.com>
2019-05-20 14:37:36 -0400

    Create commonEnd.java

---
+++ Java/Array-1/commonEnd.java
@@ -0,0 +1,12 @@
+/*
+Given 2 arrays of ints, a and b, 
+return true if they have the same first element or they have the same last element.
+Both arrays will be length 1 or more.
+*/
+
+public boolean commonEnd(int[] a, int[] b) {
+  if(a[0] == b[0] || a[a.length-1] == b[b.length-1]) 
+  	return true;
+  else 
+  	return false;
+}
