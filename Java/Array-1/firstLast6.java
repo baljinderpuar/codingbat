e24d22717812c5e9ac65c019429dfff20adc7b8c
baljinderpuar <baljinderpuar@gmail.com>
2019-05-20 10:28:15 -0400

    Update firstLast6.java

--- Java/Array-1/firstLast6.java
+++ Java/Array-1/firstLast6.java
@@ -4,9 +4,9 @@ The array will be length 1 or more.
 */
 
 public boolean firstLast6(int[] nums) {
-  if(nums[0] == 6 || nums[nums.length-1] == 6) 
-  	return true;
-  else 
-    return false;
+	if(nums[0] == 6 || nums[nums.length-1] == 6) 
+  		return true;
+  	else 
+  		return false;
 }
 	
